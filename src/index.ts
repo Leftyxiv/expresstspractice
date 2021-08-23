b878b5fd2105ecbf003e1143e9da1252ac20a5a1
Leftyxiv <manny@aledoux.net>
2021-08-23 09:01:28 -0400

    building log in route

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import cookieSession from 'cookie-session';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-  <div>
-    <h1>Hi there</h1>
-  </div>
-  `);
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['asdf'] }));
+app.use(router);
 
 app.listen(3000, () => {
   console.log('app running...')
